466acc8373a0c70127481f16a51ffb0a9d0b8989
James Huffaker <james@decisivedata.net>
2015-11-06 16:35:18 -0800

    Initial Checkin

---
+++ ViewModels/Navigation.js
@@ -0,0 +1,24 @@
+﻿define([], function () {
+    return {
+        
+        preinit: function (node, args) {
+
+        },
+
+        init: function (node, args) {
+            
+        },
+
+        deinit: function (node) {
+            
+        },
+
+        go_home: function (e) {
+            window.app.navigate("Home");
+        },
+
+        go_Temp1: function (e) {
+            window.app.navigate("Template1");
+        }
+    };
+});
---
+++ ViewModels/Template1.js
@@ -0,0 +1,21 @@
+﻿define([], function () {
+    return {
+        
+        preinit: function (node, args) {
+
+        },
+
+        init: function (node, args) {
+        	return kendo.fx(node.children()).slideIn("right").play();
+        },
+
+        deinit: function (node) {
+            return kendo.fx(node.children()).slideIn("right").reverse();
+        },
+        buildTemplate: function() {
+	        var someText = $('#someText').val();
+            var template = kendo.template('Hello Templates!</br>Sample Text: <span style="color:red;">#:data.someText#</span>');
+            $('#simpleTemplateContent').html(template({ someText: someText }));
+		}
+    };
+});
