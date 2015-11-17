926a1da78b27a7bf2e213259971f629da812be8c
James Huffaker <james@decisivedata.net>
2015-11-17 10:12:50 -0800

    Added kendo dropdownlist, bound template to viewmodel

--- ViewModels/Template2.js
+++ ViewModels/Template2.js
@@ -6,7 +6,6 @@
 		},
 
 		init: function (node, args) {
-			//kendo.bind($("#dataInput"), this.viewModel);
 			return kendo.fx(node.children()).slideIn("right").play();
 		},
 
@@ -14,17 +13,15 @@
 			return kendo.fx(node.children()).slideIn("right").reverse();
 		},
 		buildTemplate: function () {
-			var template = kendo.template($('#templateTest').html());
-			$('#simpleTemplateContent').append(template({ someText: this.viewModel.textField, dropDownValue: this.viewModel.dropDownValue }));
+			this.tableData.push({ someText: this.textField, dropDownValue: this.dropDownValue });
 		},
-		//viewModel: kendo.observable({
-			dropValues: [
-				{ Text: 'Test Value 1', Value: 1 },
-				{ Text: 'Second Test Value', Value: 2 },
-				{ Text: 'Some Third Value', Value: 3 },
-			],
-			dropDownValue: '1',
-			textField: ''
-		//})
+		dropValues: [
+			{ Text: 'Test Value 1', Value: 1 },
+			{ Text: 'Second Test Value', Value: 2 },
+			{ Text: 'Some Third Value', Value: 3 },
+		],
+		dropDownValue: '1',
+		textField: '',
+		tableData: []
 	};
 });
